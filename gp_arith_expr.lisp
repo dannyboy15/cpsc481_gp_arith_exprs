552c7dc4d768c8795e8f9427ec846b87480cf121
Daniel Bravo <bravoappdev@gmail.com>
2018-12-21 04:08:13 -0800

    Implement create_new expr

--- gp_arith_expr.lisp
+++ gp_arith_expr.lisp
@@ -1,13 +1,41 @@
+; GP Framework
+; (progn)
+;   (do until current pool filled ;; init population
+;       create new expr
+;       add expr to current pool )
+;   (do until exceed terminal generation count
+;       bump generation count
+;       (for each expr in current pool
+;            calc fitness for expr )
+;       save a copy of most fit expr for this generation
+;       (do until no more exprs in current pool
+;           select 2 exprs as parents
+;           remove parents from current pool
+;           select crossover point/node in each parent
+;           make crossed kids
+;           expose each kid to mutation
+;           add kids to next pool )
+;       current pool = next pool ))
+
 ; load support functions
 ; (load "support_fcns")
 
+; Seed the random function
+(setf *random-state* (make-random-state t))
 
 ; Global variables
 (setq MAX_GENERATIONS 5)
 (setq POPULATION_SIZE 5)
-(setq INT_RANGE '(-9 9))
+(setq RANGE_MIN -9)
+(setq RANGE_MAX 9)
 (setq OPERANDS_VARS '(x y z))
 (setq OPERATORS '(+ - *))
+(setq MAX_NUM_ARGS 4)
+
+; https://stackoverflow.com/questions/13937520/pythons-range-analog-in-common-lisp
+(defun range (max &key (min 0) (step 1))
+   (loop for n from min below max by step
+      collect n))
 
 (defun population_init ()
   (setq pool  ())
@@ -21,7 +49,32 @@
   pool)
 
 (defun create_new_expr ()
-  'hello)
+  ; pick operation
+  (setq n (random (length OPERATORS)))
+  (setq op (nth n OPERATORS))
+
+  ; pick number of args
+  (setq num_args (random MAX_NUM_ARGS))
+
+  ; create list of operands
+  (setq operands (append OPERANDS_VARS (range (1+ RANGE_MAX) :min RANGE_MIN)))
+
+  ; pick a random value for each arg and add it to expression
+  (setq expr ())
+  (setq counter 0)
+  (loop
+    (setq counter (1+ counter))
+    (setq m (random (length operands)))
+    (push (nth m operands) expr)
+    (when (>= counter num_args) (return counter)))
+
+  ; add the operator
+  (push op expr)
+
+  expr)
+
+
+
 
 (defun calculate_fitness (expr)
   'fit)
@@ -43,59 +96,44 @@
   new_pool)
 
 ; Main program
-(progn
-  (setq current_gen 0)
-  (setq best_fit_exprs ())
-  (setq gen_stats ())
+; (progn
+;   (setq current_gen 0)
+;   (setq best_fit_exprs ())
+;   (setq gen_stats ())
+;
+;   ; Initialize the poplution
+;   (setq pool (population_init))
+;   (print pool)
+;
+;   ; Run through generations
+;   (loop
+;     (setq current_gen (+ current_gen 1))
+;     (setq fitness_list ())
+;
+;     ; calculate the fitness of each expression
+;     (loop for e in pool
+;       do (push (calculate_fitness e) fitness_list)
+;       do (print fitness_list))
+;
+;     ; get the generation statistics
+;     (push (get_gen_stats current_gen fitness_list) best_fit_exprs)
+;
+;     ; find the best expression and add it to our list
+;     (push (find_best_fit_expr fitness_list) gen_stats)
+;
+;     ; get the new generation
+;     (setq pool (next_pool_gen pool current_gen))
+;     (print pool)
+;
+;     (when (>= current_gen MAX_GENERATIONS) (return current_gen)))
+;
+;   (print gen_stats) ; save to file
+;   (print best_fit_exprs))
 
-  ; Initialize the poplution
-  (setq pool (population_init))
-  (print pool)
-
-  ; Run through generations
-  (loop
-    (setq current_gen (+ current_gen 1))
-    (setq fitness_list ())
-
-    ; calculate the fitness of each expression
-    (loop for e in pool
-      do (push (calculate_fitness e) fitness_list)
-      do (print fitness_list))
-
-    ; get the generation statistics
-    (push (get_gen_stats current_gen fitness_list) best_fit_exprs)
-
-    ; find the best expression and add it to our list
-    (push (find_best_fit_expr fitness_list) gen_stats)
-
-    ; get the new generation
-    (setq pool (next_pool_gen pool current_gen))
-    (print pool)
-
-    (when (>= current_gen MAX_GENERATIONS) (return current_gen)))
-
-  (print gen_stats) ; save to file
-  (print best_fit_exprs))
+(print (population_init))
 
 
 
-; (progn)
-;   (do until current pool filled ;; init population
-;       create new expr
-;       add expr to current pool )
-;   (do until exceed terminal generation count
-;       bump generation count
-;       (for each expr in current pool
-;            calc fitness for expr )
-;       save a copy of most fit expr for this generation
-;       (do until no more exprs in current pool
-;           select 2 exprs as parents
-;           remove parents from current pool
-;           select crossover point/node in each parent
-;           make crossed kids
-;           expose each kid to mutation
-;           add kids to next pool )
-;       current pool = next pool ))
 
 ; Print out the best expr for each generation along with its score
 ; Print best, worst, avg
