4d607e5d21478bcb0be114b8f96b827dcf0a0487
Daniel Bravo <bravoappdev@gmail.com>
2018-12-21 00:53:44 -0800

    Remove tests from file

--- support_fcns.lisp
+++ support_fcns.lisp
@@ -1,6 +1,5 @@
 ;; -------------- support functions --------------------------------------------
 
-
 ;; ----------------- cell-count ------------------------------------------------
 (defun cell-count (rt)
   "Return the number of nodes/cells in the tree.  Skip non-cells."
@@ -9,13 +8,6 @@
    ((not (listp rt)) 0)
    (t (let ((cc (length rt)))
         (+ cc (apply #'+ (mapcar #'cell-count rt)))))))
-;; Tests
-;;(cell-count '(a b c))
-;;3
-;;(cell-count '(a (b) c))
-;;4
-;; (cell-count '(a))
-;; 1
 
 
 ;; --------------------------- tree-nth ----------------------------------------
@@ -45,43 +37,6 @@
          (t (tree-nth ;; Elt is in cdr subtree.
              (- rnth size1) ;; Account for skipping car subtree.
              (cdr rtree))))))))) ;; Skip car subtree.
-;; Tests
-;; (tree-nth 1.3 '(a))
-;; nil
-;; (tree-nth 1 nil)
-;; nil
-;; (tree-nth 1 'a)
-;; nil
-;; (tree-nth 1 '(a))
-;; a
-;; (tree-nth 1 '(a b c))
-;; a
-;; (tree-nth 1 '((a) b c))
-;; (a)
-;; (tree-nth 2 '(a b c))
-;; b
-;; (tree-nth 2 '(a (b) c))
-;; (b)
-;; (tree-nth 3 '(a b c))
-;; c
-;; (tree-nth 4 '(a b c))
-;; nil
-;; (tree-nth 3 '(a (b) c))
-;; b
-;; (tree-nth 3 '((a f) b c))
-;; f
-;; (tree-nth 4 '((a f) b c))
-;; b
-;; (tree-nth 5 '((a f) b c))
-;; c
-;; (tree-nth 6 '((a f) b c))
-;; nil
-;; (tree-nth 6 '((a f) b c))
-;; nil
-;; (tree-nth 6 '((a f) ((b)) c))
-;; b
-;; (tree-nth 7 '((a f) ((b)) c))
-;; c
 
 
 ;; ------------------------------------------------------- tree-nth-cell ----
@@ -105,44 +60,11 @@
                         rnth
                         (cdr rtree))) ;; Find elt in the cdr subtree.
          ((<= rnth size1) (tree-nth-cell ;;  Elt is in car subtree.
-rnth
+                           rnth
                            (car rtree))) ;; Find elt in the car subtree.
          (t (tree-nth-cell ;; Elt is in cdr subtree.
              (- rnth size1) ;; Account for skipping car subtree.
              (cdr rtree))))))))) ;; Skip car subtree.
-;; Tests
-;; (tree-nth-cell 1 nil)
-;; nil
-;; (tree-nth-cell 1 '(a b c))
-;; (a b c)
-;; (tree-nth-cell 2 '(a b c))
-;; (b c)
-;; (tree-nth-cell 3 '(a b c))
-;; (c)
-;; (tree-nth-cell 1 '((a b) c))
-;; ((a b) c)
-;; (tree-nth-cell 2 '((a b) c))
-;; (a b)
-;; (tree-nth-cell 3 '((a b) c))
-;; (b)
-;; (tree-nth-cell 4 '((a b) c))
-;; (c)
-;; (tree-nth-cell 5 '((a b) c))
-;; nil
-;; (tree-nth-cell 2 '((a f) ((b)) c))
-;; (a f)
-;; (tree-nth-cell 3 '((a f) ((b)) c))
-;; (f)
-;; (tree-nth-cell 4 '((a f) ((b)) c))
-;; (((b)) c)
-;; (tree-nth-cell 5 '((a f) ((b)) c))
-;; ((b))
-;; (tree-nth-cell 6 '((a f) ((b)) c))
-;; (b)
-;; (tree-nth-cell 7 '((a f) ((b)) c))
-;; (c)
-;; (tree-nth-cell 8 '((a f) ((b)) c))
-;; nil
 
 
 ;; ---------------------------------------------------- random-tree-cell ----
@@ -154,14 +76,6 @@ rnth
          (spot (tree-nth-cell nth rtree)))
     ;; (print (list :dbg size nth spot))
     spot))
-;; Tests
-;; (random-tree-cell '(+ (* 5 a b) (* c (- d 6))))
-;; ((* c (- d 6))) ;; No op
-;; (- d 6) ;; Has op
-;; ((* 5 a b) (* c (- d 6))) ;; No op
-;; (* 5 a b) ;; Has op
-;; (+ (* 5 a b) (* c (- d 6))) ;; Has op
-;; (* c (- d 6)) ;; Has op
 
 
 ;; ------------------------------------------------------------ make-kid ----
@@ -190,97 +104,42 @@ rnth
               (if (member (car tgt) ops) ;; Tgt also has an op?
                   newop
                 newnop))))
-;; Tests
-;; (cell-count '(+ (* 5 a b) (* c (- d 6))))
-;; 13
-;; (test-make-kid '(+ (* 5 a b) (* c (- d 6))))
-;; (:dbg :tgt (c (- d 6)))
-;; (+ (* 5 a b) (* 8 9))
-;; (:dbg :tgt ((- d 6)))
-;; (+ (* 5 a b) (* c 8 9))
-;; (:dbg :tgt ((- d 6)))
-;; (+ (* 5 a b) (* c 8 9))
-;; (:dbg :tgt (d 6))
-;; (+ (* 5 a b) (* c (- 8 9)))
-;; (:dbg :tgt (5 a b))
-;; (+ (* 8 9) (* c (- d 6)))
-;; (:dbg :tgt (a b))
-;; (+ (* 5 8 9) (* c (- d 6)))
-;; (:dbg :tgt ((* 5 a b) (* c (- d 6))))
-;; (+ 8 9)
-;; (:dbg :tgt ((- d 6)))
-;; (+ (* 5 a b) (* c 8 9))
-;; (:dbg :tgt (6))
-;; (+ (* 5 a b) (* c (- d 8 9)))
 
 
 ;; -------------------------------------------------- get-front-upto-nth ----
-(defun get-front-upto-nth ( rn rlist )
+(defun get-front-upto-nth ( rn rlist)
   "Return list head from 0-th thru N-th elt.  Assumes elt-n is unique."
   (let ((elt-n (nth rn rlist)))
     (reverse (member elt-n (reverse rlist)))))
-;; Tests
-;; (setq my-list '((1 a) (2 b) (3 c) (4 d) (5 e) (6 f) (7 g)))
-;; (get-front-from-nth 4 my-list)
-;; ((1 a) (2 b) (3 c) (4 d) (5 e))
-;; (get-front-from-nth 2 my-list)
-;; ((1 a) (2 b) (3 c))
 
 
 ;; ---------------------------------------------------------- get-score ----
 (defun get-score (rcritter)
   "Get score for critter.  Dummy fcn: just return its length."
   (length rcritter))
-;; Tests
-;; (get-score '(+ 3 4))
-;; 3
 
 
 ;; ---------------------------------------------------------- score-pop ----
-(defun score-pop ( rpop ) ;; Pop is a population.
+(defun score-pop ( rpop) ;; Pop is a population.
   "Create Pop-Scored pairs (Score Critter) given Pop list of critters."
   (mapcar #'(lambda (critter)
               (let ((score (get-score critter)))
                 (list score critter)))
           rpop))
-;; Tests
-;; (setq my-pop '((a b c)
-;; (a)
-;; (e f g)
-;;                (a d)))
-;; ((a b c) (a) (e f g) (a d))
-;; (setq my-pop-scored (score-pop my-pop))
-;; ((3 (a b c)) (1 (a)) (3 (e f g)) (2 (a d)))
+
+
 ;; ------------------------------------------------ safe-sort-scored-pop ----
-(defun safe-sort-scored-pop ( rscored-pop )
+(defun safe-sort-scored-pop ( rscored-pop)
   "Return a sorted list of scored-critter elts.  Don't change given list.
    NB, your Lisp's built-in sort fcn may damage the incoming list."
   (let ((sacrifice-list (copy-list rscored-pop)))
     (sort sacrifice-list
           #'(lambda (scored-critter-1 scored-critter-2)
               (< (car scored-critter-1) (car scored-critter-2))))))
-;; Tests
-;; my-pop-scored
-;; ((3 (a b c)) (1 (a)) (3 (e f g)) (2 (a d)))
-;; (safe-sort-scored-pop my-pop-scored)
-;; ((1 (a)) (2 (a d)) (3 (a b c)) (3 (e f g)))
-;; my-pop-scored
-;; ((3 (a b c)) (1 (a)) (3 (e f g)) (2 (a d)))
+
+
 ;; ------------------------------------------------- get-pop-from-scored ----
 (defun get-pop-from-scored (rscored-pop)
   "Return just the Pop of critters from the Scored Pop."
   ;;Alt: (mapcar #'(lambda (elt) (nth 1 elt)) rscored-pop)
   (mapcar #'cadr rscored-pop))
-;; Tests
-;; my-pop-scored
-;; ((3 (a b c)) (1 (a)) (3 (e f g)) (2 (a d)))
-;; (get-pop-from-scored my-pop-scored)
-;; ((a b c) (a) (e f g) (a d))
-
-;; ----------------------------------------------------------- deep-copy ----
-; (defun deepcopy ( rn )
-;;  (cond
-;;    ((not (listp rn)) rn)
-;;   (T (let ((lx (deepcopy(car rn)))
-;;             (rx (deepcopy (cdr rn))))
-;;            (cons lx rx)))))
